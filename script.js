5715b2e44f59b9c1fd1e7dd1fdff4b166786036a
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-01-17 01:36:16 -0500

    Add all possible characters in an array

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ function passLength() {
     return parseInt(prompt('How long do you want your password to be?'));
 }
 
-console.log(passLength())
+let length = passLength()
+const allChar = ['1','!', '2', '@','3','#','4','$','5','%','6','^','7','&','8','*',
+'9','(',')','-','_','+','=','q','Q','w','W','e','E','r','R','T','t','y','Y'
+,'u','U','i','I','o','O','P','p','[',']','{','}','a','A','s','S','d','D','f',
+'F','g','G','h','H','j','J','k','K','l','L',';',':','"',",",'z','Z','x','X',
+'c','`','~','v','b','B','n','N','m','M',',','<','>','.','>','/','?','\\','|'];
+const randomChar = allChar[Math.floor(Math.random() * allChar.length)];
+
+console.log(randomChar)
+
+
+
