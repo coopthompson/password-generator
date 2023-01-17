3e2ea7e71e70ec85eb07820fa61a8351ffa5dfe9
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-01-17 13:31:44 -0500

    Change variable and function names
    I changed the variable and function names to start with nouns or
    adjectives and verbs respectively. This is a more uniform way of
    organization I will be pursuing for future scripts.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-function passGenerator(length) {
+function generatePass(length) {
     let AllChars = [];
     for (let i=32; i<127; i++) {
         AllChars.push(String.fromCharCode(i));
     }
     let result = '';
     let characters = AllChars.join('')
-    let trueChar = characters.trim()
-    let charactersLength = trueChar.length;
+    let trueCharacters = characters.trim()
+    let charactersLength = trueCharacters.length;
     for ( i = 0; i < length; i++ ) {
-        result += trueChar.charAt(Math.floor(Math.random() * charactersLength));
+        result += trueCharacters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
 
 let trueLength = parseInt(prompt('How long do you want your password?'))
 
-console.log(passGenerator(trueLength))
+console.log(generatePass(trueLength))
 
 
 
