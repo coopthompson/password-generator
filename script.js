80b3e4d76159cc4dd7f4ba507f4c8a50cf02e423
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-01-27 00:09:40 -0500

    Add checkboxes
    Added checkboxes to control for numbers, special characters, and
    capitalized letters. This allows the user to have greater customization
    for their passwords.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const slider = document.getElementById("myRange");
 const output = document.querySelector(".slideDisplay");
 const passwordDisplay = document.querySelector(".displayBox");
+const numberCheck = document.querySelector("#numbers");
+const capitalCheck = document.querySelector("#capitalLetters");
+const specialCheck = document.querySelector("#specialCharacters");
 
 
 
@@ -8,17 +11,42 @@ const passwordDisplay = document.querySelector(".displayBox");
 
 function generatePass(length) {
     let AllChars = [];
-    for (let i=33; i<127; i++) {
-        AllChars.push(String.fromCharCode(i));
+    if (capitalCheck.checked === true) {
+        for (let i=65; i<=90; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
+    };
+    if (numberCheck.checked === true) {
+        for (let i=48; i<=57; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
+    };
+    if (specialCheck.checked === true) {
+        for (let i=32; i<=47; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
+        for (let i=58; i<=64; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
+        for (let i=91; i<=96; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
+        for (let i=123; i<127; i++) {
+            AllChars.push(String.fromCharCode(i));
+        };
     }
+    for (let i=97; i<=122; i++) {
+        AllChars.push(String.fromCharCode(i));
+    };
     let result = '';
     let characters = AllChars.join('')
     let charactersLength = characters.length;
     for ( i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+    };
     return result;
-}
+};
+  
 
 
 slider.addEventListener('mouseover', function changeOutputText() {
